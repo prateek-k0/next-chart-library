93f7f1afcdd21a576898f4d5b5c548ed79002266
Prateek Katkar <parx1601@gmail.com>
2024-05-08 21:53:13 +0530

    scroll styles added

--- src/components/common/CodeBlock.tsx
+++ src/components/common/CodeBlock.tsx
@@ -5,7 +5,7 @@ import { CopyBlock, dracula } from 'react-code-blocks';
 
 const CodeBlock = ({ scriptText }: { scriptText: string }) => {
   return (
-    <div className="rounded-lg overflow-hidden">
+    <div className="rounded-lg overflow-hidden code-block">
       <div className='max-h-[2880px] overflow-y-auto'>
         <CopyBlock 
           text={scriptText}
