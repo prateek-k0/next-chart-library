02b3ac9345ee26f95b1ee44b707d6e4bf03cd302
Prateek Katkar <81347315+prateek-k0@users.noreply.github.com>
2024-07-06 08:20:09 +0530

    Added refernce for ridgeline

--- src/app/charts/area-charts/ridge-line/ChartUtil.tsx
+++ src/app/charts/area-charts/ridge-line/ChartUtil.tsx
@@ -1,4 +1,5 @@
 "use client";
+// reference: https://d3-graph-gallery.com/graph/ridgeline_basic.html
 import { ChartProps } from "@/types";
 import React, { useRef, useCallback, useEffect } from "react";
 import { useResizeObserver } from "@/hooks/useResizeObserver";
