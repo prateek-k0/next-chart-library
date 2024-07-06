212a8d7ee869c3669b3ef6d5328b86766a28f66c
Prateek Katkar <81347315+prateek-k0@users.noreply.github.com>
2024-07-06 08:49:59 +0530

    Added reference for brushed scatter

--- src/app/charts/scatter-plots/brush/ChartUtil.tsx
+++ src/app/charts/scatter-plots/brush/ChartUtil.tsx
@@ -1,4 +1,5 @@
 "use client";
+// reference: https://d3-graph-gallery.com/graph/interactivity_brush.html
 import React, { useEffect, useCallback, useRef } from "react";
 import * as d3 from "d3";
 import { useResizeObserver } from "@/hooks/useResizeObserver";
